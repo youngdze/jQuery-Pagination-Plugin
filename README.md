40f4e61781a82fb6cf837ebf9452f404fb1bf418
youngdze <youngdze94@gmail.com>
2014-07-11 19:20:47 +0800

    ver 1.0

---
+++ README.md
@@ -0,0 +1,11 @@
+JavaScript 分页程序
+===
+
+用法：
+---
+
+$(selector).pagination()
+
+.pagination() 可以接受一个正整数参数作为总页数，无参数时总页数默认为 20。
+
+当前页面由 location.hash 控制，location.hash 为空时当前页面为 1。
--- js/pagination.js
+++ js/pagination.js
@@ -1,176 +1,195 @@
-// generate pagination quene
-var Pagination = function(total, currentPage) {
+(function($, window, document, undefined) {
 
-    this.total = total;
-    this.currentPage = currentPage;
+    // generate pagination quene
+    var Pagination = function(total, currentPage) {
 
-    this.generate = function() {
+        this.total = total;
+        this.currentPage = currentPage;
 
-        try {
+        this.generate = function() {
 
-            // validate if both are positive integers
-            function isInteger(data) {
-                var exgInt = /^\+?(0|[1-9])\d*$/;
-                return exgInt.test(data);
-            }
+            try {
 
-            if (!(isInteger(this.total) || isInteger(this.currentPage))) {
-                throw 'positive integers required';
-            }
+                // validate if both are positive integers
+                function isInteger(data) {
+                    var exgInt = /^\+?(0|[1-9])\d*$/;
+                    return exgInt.test(data);
+                }
 
-            if (this.currentPage > this.total) {
-                throw 'current index of page cannot bigger than the total number of pages';
-            }
+                if (!(isInteger(this.total) || isInteger(this.currentPage))) {
+                    throw 'positive integers required';
+                }
 
-            var pageButton = [];
+                if (this.currentPage > this.total) {
+                    throw 'current index of page cannot bigger than the total number of pages';
+                }
 
-            // The first situation
-            if (this.currentPage < 6) {
-                for (var i = 1; i <= this.total; i++) {
-                    if (i <= (this.currentPage + 2)) {
-                        pageButton.push(i);
-                    } else if (Math.abs(((this.currentPage + 2) - this.total)) < 2) {
-                        pageButton.push(i);
-                        if (i === this.total) {
-                            break;
+                var pageButton = [];
+
+                // The first situation
+                if (this.currentPage < 6) {
+                    for (var i = 1; i <= this.total; i++) {
+                        if (i <= (this.currentPage + 2)) {
+                            pageButton.push(i);
+                        } else if (Math.abs(((this.currentPage + 2) - this.total)) < 2) {
+                            pageButton.push(i);
+                            if (i === this.total) {
+                                break;
+                            }
+                        } else if (i === this.total) {
+                            pageButton.push('...');
+                            pageButton.push(this.total);
+                        } else {
+                            continue;
                         }
-                    } else if (i === this.total) {
-                        pageButton.push('...');
-                        pageButton.push(this.total);
-                    } else {
-                        continue;
                     }
                 }
-            }
 
-            // The second situation
-            else if (this.currentPage >= 6 && this.currentPage <= (this.total - 5)) {
-                for (var i = 1; i <= this.total; i++) {
-                    if (i === 1) {
-                        pageButton.push(1);
-                        pageButton.push('...');
-                    } else if (i < (this.currentPage - 2)) {
-                        continue;
-                    } else if (i >= (this.currentPage - 2) && i <= (this.currentPage + 2)) {
-                        pageButton.push(i);
-                    } else if (i > (this.currentPage + 2) && i < this.total) {
-                        continue;
-                    } else if (i === this.total) {
-                        pageButton.push('...');
-                        pageButton.push(i);
+                // The second situation
+                else if (this.currentPage >= 6 && this.currentPage <= (this.total - 5)) {
+                    for (var i = 1; i <= this.total; i++) {
+                        if (i === 1) {
+                            pageButton.push(1);
+                            pageButton.push('...');
+                        } else if (i < (this.currentPage - 2)) {
+                            continue;
+                        } else if (i >= (this.currentPage - 2) && i <= (this.currentPage + 2)) {
+                            pageButton.push(i);
+                        } else if (i > (this.currentPage + 2) && i < this.total) {
+                            continue;
+                        } else if (i === this.total) {
+                            pageButton.push('...');
+                            pageButton.push(i);
+                        }
                     }
                 }
-            }
 
-            // The third situation
-            else if (this.currentPage > (this.total - 5)) {
-                for (var i = 1; i <= this.total; i++) {
-                    if (i === 1) {
-                        pageButton.push(1);
-                        pageButton.push('...');
-                    } else if (i < (this.currentPage - 2)) {
-                        continue;
-                    } else {
-                        pageButton.push(i);
+                // The third situation
+                else if (this.currentPage > (this.total - 5)) {
+                    for (var i = 1; i <= this.total; i++) {
+                        if (i === 1) {
+                            pageButton.push(1);
+                            pageButton.push('...');
+                        } else if (i < (this.currentPage - 2)) {
+                            continue;
+                        } else {
+                            pageButton.push(i);
+                        }
                     }
                 }
+
+                if (this.currentPage === 1) {
+                    if (this.total !== 1) {
+                        pageButton.push('next');
+                    }
+                } else if (this.currentPage === this.total) {
+                    pageButton.splice(0, 0, 'pre');
+                } else {
+                    pageButton.splice(0, 0, 'pre');
+                    pageButton.push('next');
+                }
+
+                return pageButton;
+
+            } catch (err) {
+                console.log('Error: ' + err + ".");
             }
-
-            if (this.currentPage === 1) {
-                pageButton.push('next');
-            } else if (this.currentPage === this.total) {
-                pageButton.splice(0, 0, 'pre');
-            } else {
-                pageButton.splice(0, 0, 'pre');
-                pageButton.push('next');
-            }
-
-            return pageButton;
-
-        } catch (err) {
-            console.log('Error: ' + err + ".");
-        }
-    };
-}
-
-// generate normal element
-var Element = function(nodeName, text) {
-
-    this.nodeName = nodeName;
-    this.text = text;
-
-    this.generate = function() {
-
-        var newElement = document.createElement(this.nodeName);
-        var newContent = document.createTextNode(this.text);
-        newElement.appendChild(newContent);
-
-        return newElement;
-    }
-}
-
-var renderPage = function(totalPage, currentPage) {
-
-    var hash = parseInt(location.hash.substring(6), 10);
-
-    var page = new Pagination(totalPage, currentPage);
-    var pageQuene = page.generate();
-
-    var ul_element = new Element('ul', '');
-    var ul = ul_element.generate();
-    ul.setAttribute('class', 'pagination');
-
-    for (var i = 0; i < pageQuene.length; i++) {
-        var li_element = new Element('li', '');
-        var a_element = new Element('a', pageQuene[i]);
-        var li = li_element.generate();
-        var a = a_element.generate();
-
-        if (pageQuene[i] === 'pre' || pageQuene[i] === 'next' || pageQuene[i] === '...') {
-            a.setAttribute('href', 'javascript:void(0)');
-
-            if (pageQuene[i] === 'pre') {
-                li.setAttribute('class', 'pagePre');
-            } else if (pageQuene[i] === 'next') {
-                li.setAttribute('class', 'pageNext');
-            } else {
-                a.setAttribute('style', 'cursor: not-allowed;');
-            }
-
-        } else {
-            a.setAttribute('href', '#page=' + pageQuene[i]);
-        }
-
-        if (!hash && pageQuene[i] == 1) {
-            li.setAttribute('class', 'active');
-        } else if (pageQuene[i] === hash) {
-            li.setAttribute('class', 'active');
-        }
-
-        li.appendChild(a);
-        ul.appendChild(li);
+        };
     }
 
-    return ul;
-}
+    // generate normal element
+    var Element = function(nodeName, text) {
 
-$(window).load(function() {
-    var pageNow = parseInt(location.hash.substring(6), 10) ? parseInt(location.hash.substring(6), 10) : 1;
-    $('.pagewrapper').html(renderPage(20, pageNow));
-    $('.pagination').find('li').click(function(e) {
-        if ($(this).hasClass('pagePre')) {
-            window.location = window.location.pathname + "#page=" + (--pageNow);
-            location.reload();
-        } else if ($(this).hasClass('pageNext')) {
-            window.location = window.location.pathname + "#page=" + (++pageNow);
-            location.reload();
-        } else if ($(this).text() === '...') {
+        this.nodeName = nodeName;
+        this.text = text;
 
-        } else if (parseInt($(this).find('a').attr('href').substring(6), 10) === pageNow) {
+        this.generate = function() {
 
-        }else {
-            window.location = window.location.pathname + "#page=" + parseInt($(this).find('a').attr('href').substring(6), 10);
-            location.reload();
+            var newElement = document.createElement(this.nodeName);
+            var newContent = document.createTextNode(this.text);
+            newElement.appendChild(newContent);
+
+            return newElement;
         }
-    });
-});
+    }
+
+    var renderPage = function(totalPage, currentPage) {
+
+        var hash = parseInt(location.hash.substring(6), 10);
+
+        var page = new Pagination(totalPage, currentPage);
+        var pageQuene = page.generate();
+
+        var ul_element = new Element('ul', '');
+        var ul = ul_element.generate();
+        ul.setAttribute('class', 'pagination');
+
+        for (var i = 0; i < pageQuene.length; i++) {
+            var li_element = new Element('li', '');
+            var a_element = new Element('a', pageQuene[i]);
+            var li = li_element.generate();
+            var a = a_element.generate();
+
+            if (pageQuene[i] === 'pre' || pageQuene[i] === 'next' || pageQuene[i] === '...') {
+                a.setAttribute('href', 'javascript:void(0)');
+
+                if (pageQuene[i] === 'pre') {
+                    li.setAttribute('class', 'pagePre');
+                } else if (pageQuene[i] === 'next') {
+                    li.setAttribute('class', 'pageNext');
+                } else {
+                    a.setAttribute('style', 'cursor: not-allowed;');
+                }
+
+            } else {
+                a.setAttribute('href', '#page=' + pageQuene[i]);
+            }
+
+            if (!hash && pageQuene[i] === 1) {
+                li.setAttribute('class', 'active');
+            } else if (pageQuene[i] === hash) {
+                li.setAttribute('class', 'active');
+            }
+
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+
+        return ul;
+    }
+
+    $.fn.pagination = function(option) {
+
+        var _default = 20;
+        var $this = $(this);
+
+        _default = option ? option : _default;
+
+        var pageNow = parseInt(location.hash.substring(6), 10) ? parseInt(location.hash.substring(6), 10) : 1;
+        $this.html(renderPage(_default, pageNow));
+
+        (function selectPage() {
+            var _li = $('.pagination').find('li');
+            _li.live('click', function() {
+                var _this = $(this);
+                if (_this.hasClass('pagePre')) {
+                    pageNow--;
+                    location.hash = "#page=" + pageNow;
+                    $this.html(renderPage(_default, pageNow));
+                } else if (_this.hasClass('pageNext')) {
+                    pageNow++;
+                    location.hash = "#page=" + pageNow;
+                    $this.html(renderPage(_default, pageNow));
+                } else if (_this.text() === '...') {
+
+                } else {
+                    pageNow = parseInt(_this.find('a').attr('href').substring(6), 10);
+                    location.hash = "#page=" + pageNow;
+                    $this.html(renderPage(_default, pageNow));
+                }
+            });
+        })();
+
+    }
+
+})(jQuery, window, document);
